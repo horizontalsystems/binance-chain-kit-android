3476727666d7f9a5b96b3115b424bb4ff5c65305
nk-the-crazy <nurkali@gmail.com>
2019-07-29 12:57:53 +0600

    Initial commit.

---
+++ binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/BinanceChainKit.kt
@@ -0,0 +1,10 @@
+package io.horizontalsystems.binancechainkit.core
+
+import java.util.logging.Logger
+
+
+class BinanceChainKit
+{
+    private val logger = Logger.getLogger("BinanceChainKit")
+
+}
---
+++ binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/Interfaces.kt
@@ -0,0 +1,9 @@
+package io.horizontalsystems.binancechainkit.core
+
+
+
+interface Blockchain
+{
+
+}
+
