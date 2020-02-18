e9c7a27b3667ea0a9fb0eed8fdd36ca6bf8d141f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-02-18 10:48:49 +0600

    Parse HttpException returned by Binance

--- binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/api/Errors.kt
+++ binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/api/Errors.kt
@@ -1,10 +1,20 @@
 package io.horizontalsystems.binancechainkit.core.api
 
+import com.google.gson.annotations.SerializedName
 
 
 sealed class BinanceException(message: String) : IllegalArgumentException(message) {
 
     class InvalidType(errorMessage: String) :
         BinanceException("Invalid Type:$errorMessage" )
+}
 
+class BinanceError: Exception() {
+    var code: String = ""
+
+    @SerializedName("failed_tx_index")
+    val failedTxIndex: String = ""
+
+    @SerializedName("message")
+    override var message: String = ""
 }
