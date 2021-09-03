fe2a3be7aebe00df1d42b683b7e0e195d1af856d
bakyt <abdrasulov@gmail.com>
2021-09-03 16:11:21 +0600

    Add ability to subscribe for transaction updates with specific type

--- binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/Asset.kt
+++ binancechainkit/src/main/java/io/horizontalsystems/binancechainkit/core/Asset.kt
@@ -1,12 +1,13 @@
 package io.horizontalsystems.binancechainkit.core
 
+import io.horizontalsystems.binancechainkit.models.TransactionFilterType
 import io.horizontalsystems.binancechainkit.models.TransactionInfo
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.subjects.PublishSubject
 import java.math.BigDecimal
 
-class Asset(val symbol: String) {
+class Asset(val symbol: String, private val account: String) {
 
     var balance: BigDecimal = BigDecimal(0)
         set(value) {
@@ -17,8 +18,25 @@ class Asset(val symbol: String) {
     val balanceFlowable: Flowable<BigDecimal>
         get() = balanceSubject.toFlowable(BackpressureStrategy.BUFFER)
 
-    val transactionsFlowable: Flowable<List<TransactionInfo>>
-        get() = transactionsSubject.toFlowable(BackpressureStrategy.BUFFER)
+    fun getTransactionsFlowable(filterType: TransactionFilterType? = null): Flowable<List<TransactionInfo>> {
+        val observable = when (filterType) {
+            null -> {
+                transactionsSubject
+            }
+            TransactionFilterType.Incoming -> {
+                transactionsSubject
+                    .map { it.filter { it.to == account } }
+                    .filter { it.isNotEmpty() }
+            }
+            TransactionFilterType.Outgoing -> {
+                transactionsSubject
+                    .map { it.filter { it.from == account } }
+                    .filter { it.isNotEmpty() }
+            }
+        }
+
+        return observable.toFlowable(BackpressureStrategy.BUFFER)
+    }
 
     val transactionsSubject = PublishSubject.create<List<TransactionInfo>>()
 
